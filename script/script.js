793b618e054b8abd6af1c809a0b3a3e3ba22f129
Roman Timakov <romaintimakoff@yandex.ru>
2020-12-01 21:07:22 +0300

    3

--- script/script.js
+++ script/script.js
@@ -3,7 +3,6 @@ let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__editbutton');
 
 let profileTitleNode = document.querySelector('.profile__title');
-let formButtonNode = document.querySelector('.form__button');
 let profileParagraphNode = document.querySelector('.profile__paragraph');
 
 let elementHeaderLikes = document.querySelectorAll('.element__like');
@@ -13,25 +12,23 @@ let forms = document.querySelector('.form');
 let firstNameInput = document.getElementById('1');
 let secondNameInput = document.getElementById('2');
 
-firstNameInput.value = profileTitleNode.textContent;
-secondNameInput.value = profileParagraphNode.textContent;
-
 
 editButton.addEventListener('click', togglePopupVisibility);
 closeButton.addEventListener('click', togglePopupVisibility);
 
 function togglePopupVisibility() {
     popup.classList.toggle('popup_opened');
-
+    firstNameInput.value = profileTitleNode.textContent;
+    secondNameInput.value = profileParagraphNode.textContent;
 }
 
 forms.addEventListener('submit', handleFormSubmit);
-forms.addEventListener('submit', togglePopupVisibility);
 
 function handleFormSubmit(event) {
     event.preventDefault();
     profileTitleNode.textContent = firstNameInput.value;
     profileParagraphNode.textContent = secondNameInput.value;
+    togglePopupVisibility();
 }
 
 elementHeaderLikes.forEach((elementHeaderLike) => {
